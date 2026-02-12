82cee7c5f9969d622ca73afe8fb0cb5e382a63b7
aldia_mechdream <futami16237@gmail.com>
2026-02-13 07:18:01 +0800

    feat: 继续简化rollup插件配置

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,6 @@
 // @ts-check
 import path from 'node:path';
-import { readdirSync, readFileSync } from 'node:fs';
-import { type RollupReplaceOptions } from '@rollup/plugin-replace';
+import { existsSync } from 'node:fs';
 
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
@@ -10,94 +9,21 @@ import alias from '@rollup/plugin-alias';
 import terser from '@rollup/plugin-terser';
 import replace from '@rollup/plugin-replace';
 import dts from 'rollup-plugin-dts';
+import { getRollupAliases } from '.scripts/aliases.js';
+import { replaceOpts } from '.scripts/replace-options.js';
 
-interface CommonPackageJson {
-  name: string;
-  version: string;
-  description: string;
-  description_zh: string;
-  author: {
-    name: string;
-    email: string;
-  };
-  license: string;
-  repository: {
-    type: string;
-    url: string;
-  };
-}
-
-function replaceOpts(packagePath: string | undefined) {
-  if (!packagePath) {
-    console.error('Error: LIB_PACKAGE_PATH environment variable is not set.');
-    process.exit(1);
+const getTsConfigPath = (packagePath: string) => {
+  const tsconfigPath1 = path.join(packagePath, 'tsconfig.build.json');
+  if (existsSync(tsconfigPath1)) {
+    return tsconfigPath1;
   }
-
-  const pkg = JSON.parse(readFileSync(path.join(packagePath, 'package.json'), 'utf-8')) as CommonPackageJson;
-
-  function formatDateFull(dt = new Date()) {
-    const y = dt.getFullYear();
-    const m = String(dt.getMonth() + 1).padStart(2, '0');
-    const d = String(dt.getDate()).padStart(2, '0');
-    const hh = String(dt.getHours()).padStart(2, '0');
-    const mm = String(dt.getMinutes()).padStart(2, '0');
-    const ss = String(dt.getSeconds()).padStart(2, '0');
-    const ms = String(dt.getMilliseconds()).padStart(3, '0');
-    return `${y}.${m}.${d} ${hh}:${mm}:${ss}.${ms}`;
-  }
-
-  const __KEBAB_NAME__ = pkg.name.replace('rollup-plugin-', '');
-  const __VERSION__ = pkg.version;
-  const __NAME__ = __KEBAB_NAME__.replace(/(^|-)(\w)/g, (_, __, c) => c.toUpperCase());
-
-  const __PKG_INFO__ = `## About
- * @package ${__NAME__}
- * @author ${pkg.author.name} <${pkg.author.email}>
- * @version ${pkg.version} (Last Update: ${formatDateFull()})
- * @license ${pkg.license}
- * @link ${pkg.repository.url}
- * @link https://baendlorel.github.io/ Welcome to my site!
- * @description ${pkg.description.replace(/\n/g, '\n * \n * ')}
- * @copyright Copyright (c) ${new Date().getFullYear()} ${pkg.author.name}. All rights reserved.`;
-
-  const opts: RollupReplaceOptions = {
-    preventAssignment: true,
-    delimiters: ['', ''],
-    values: {
-      __IS_DEV__: 'false',
-      __NAME__,
-      __KEBAB_NAME__,
-      __PKG_INFO__,
-      __VERSION__,
-
-      // global error/warn/debug
-      "$throw('": `throw new Error('[${__NAME__} error] `,
-      '$throw(`': `throw new Error(\`[${__NAME__} error] `,
-      '$throw("': `throw new Error("[${__NAME__} error] `,
-      '$warn(': `console.warn('[${__NAME__} warn]',`,
-      '$error(': `console.error('[${__NAME__} error]',`,
-      '$debug(': `console.debug('[${__NAME__} debug]',`,
-    },
-  };
-
-  return opts;
-}
-
-const packagesDir = path.resolve('packages');
-function getRollupAliases() {
-  const packageDirs = readdirSync(packagesDir);
-  const alias: { entries: Array<{ find: RegExp | string; replacement: string }> } = { entries: [] };
-  for (const dir of packageDirs) {
-    alias.entries.push({
-      find: new RegExp(`^@${dir}`),
-      replacement: path.join(packagesDir, dir, 'src'),
-    });
-  }
-  return alias;
-}
+  return path.join(packagePath, 'tsconfig.json');
+};
 
 export default () => {
   const currentPackagePath = process.env.LIB_PACKAGE_PATH || '';
+  const tsconfigPath = getTsConfigPath(currentPackagePath);
+  console.log(`Building package at ${currentPackagePath} using tsconfig: ${tsconfigPath}`);
 
   return [
     // * Main
@@ -117,7 +43,7 @@ export default () => {
         replace(replaceOpts(currentPackagePath)),
         resolve(),
         commonjs(),
-        typescript(),
+        typescript({ tsconfig: tsconfigPath }),
         terser({
           format: {
             comments: false, // remove comments
@@ -140,7 +66,7 @@ export default () => {
     {
       input: path.join(currentPackagePath, 'src', 'index.ts'),
       output: [{ file: path.join(currentPackagePath, 'dist', 'index.d.ts'), format: 'es' }],
-      plugins: [alias(getRollupAliases()), replace(replaceOpts(currentPackagePath)), dts()],
+      plugins: [alias(getRollupAliases()), replace(replaceOpts(currentPackagePath)), dts({ tsconfig: tsconfigPath })],
     },
   ];
 };
