6ddc39980c78f270241303b8295d8783bd983b3a
aldia_mechdream <futami16237@gmail.com>
2026-02-13 00:45:02 +0800

    chore: 待修复vitest错误

--- .scripts/test.ts
+++ .scripts/test.ts
@@ -1,13 +1,18 @@
 import { execSync } from 'node:child_process';
-import { dirname } from 'node:path';
+import { dirname, resolve } from 'node:path';
 import { getPackageInfo } from './common/package-info.js';
 
 export async function test(who: string | undefined) {
   const info = getPackageInfo(who);
   const env = { ...process.env, LIB_PACKAGE_PATH: dirname(info.path) };
 
+  const vitestConfigPath = resolve('vitest.config.ts');
+  const testPackageDir = resolve(info.path, '..', 'tests');
+
+  console.log('testPackageDir', testPackageDir);
+
   if (info.json.scripts?.test) {
-    execSync(`pnpm --filter ${info.name} test`, { stdio: 'inherit', env });
+    execSync(`vitest ${testPackageDir} --config ${vitestConfigPath}`, { stdio: 'inherit', env });
   } else {
     console.warn(`Package ${info.name} has no test script, skipping...`);
   }
