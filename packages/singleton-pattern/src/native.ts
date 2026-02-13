add063822c93bee307d3f3067dd595f5412dd211
aldia_mechdream <futami16237@gmail.com>
2026-02-13 16:46:35 +0800

    feat: 1.2.0 发布，精简的singleton

--- packages/singleton-pattern/src/native.ts
+++
@@ -1,3 +0,0 @@
-export const $setPrototypeOf = Reflect.setPrototypeOf;
-export const $define = Reflect.defineProperty;
-export const $get = Reflect.get;
--- packages/singleton-pattern/src/singleton-wrapper.ts
+++ packages/singleton-pattern/src/singleton-wrapper.ts
@@ -1,42 +1,5 @@
-class Singletonify {
-  private readonly p2c = new WeakMap<ProxiedClass, Class>();
-  private readonly c2p = new WeakMap<Class, ProxiedClass>();
-
-  singletonify<T extends Class>(target: T, options?: SingletonifyOptions): T {
-    const { changeProtoConstructor = true, onlyOnce = true } = Object(options);
-
-    if (onlyOnce && this.c2p.has(target)) {
-      return this.c2p.get(target) as T;
-    }
-
-    let instance: any;
-    const proxied = new Proxy(target, {
-      construct(cls, args) {
-        if (!instance) {
-          instance = new cls(...args);
-        }
-        return instance;
-      },
-    });
-
-    if (changeProtoConstructor) {
-      proxied.prototype.constructor = proxied;
-    }
-
-    if (onlyOnce) {
-      this.p2c.set(proxied, target);
-      this.c2p.set(target, proxied);
-    }
-
-    return proxied;
-  }
-
-  getSingletonTarget<T extends Class>(singleton: T): T | undefined {
-    return this.p2c.get(singleton) as T | undefined;
-  }
-}
-
-const instance = new Singletonify();
+const proxiedToTarget = new WeakMap<ProxiedClass, Class>();
+const targetToProxied = new WeakMap<Class, ProxiedClass>();
 
 /**
  * ## Usage
@@ -48,11 +11,45 @@ const instance = new Singletonify();
  *
  * __PKG_INFO__
  */
-export const singletonify = instance.singletonify.bind(instance);
+export function singletonify<T extends Class>(target: T, options?: SingletonifyOptions): T {
+  const opts = Object(options);
+  const changeProtoConstructor = opts.changeProtoConstructor ?? true;
+  const onlyOnce = opts.onlyOnce ?? true;
+
+  if (onlyOnce) {
+    const cached = targetToProxied.get(target);
+    if (cached) {
+      return cached as T;
+    }
+  }
+
+  let instance: object | undefined;
+  const proxied = new Proxy(target, {
+    construct(cls, args): object {
+      if (!instance) {
+        instance = new cls(...args) as object;
+      }
+      return instance;
+    },
+  }) as T;
+
+  if (changeProtoConstructor) {
+    proxied.prototype.constructor = proxied;
+  }
+
+  if (onlyOnce) {
+    proxiedToTarget.set(proxied as ProxiedClass, target);
+    targetToProxied.set(target, proxied as ProxiedClass);
+  }
+
+  return proxied;
+}
 
 /**
  * Retrieves the original class from the singletonified class
  * @param singleton The singletonified class
  * @returns `undefined` if the class is not singletonified, otherwise returns the original class
  */
-export const getSingletonTarget = instance.getSingletonTarget.bind(instance);
+export const getSingletonTarget = <T extends Class>(singleton: T): T | undefined => {
+  return proxiedToTarget.get(singleton) as T | undefined;
+};
