1a69f32fdd3182f5cfba1e21c70e0ca7fc7378e9
aldia_mechdream <futami16237@gmail.com>
2026-02-13 15:40:43 +0800

    feat: 概率分支优化，去掉privatesymbol，不需要这样严防死守； warn和expect处理

--- packages/probability-branch/src/expect.ts
+++ packages/probability-branch/src/expect.ts
@@ -1,9 +1,9 @@
-export function $expect(o: unknown, message: string): void {
+export const $expect = (o: unknown, message: string): void => {
   if (!o) {
     throw new Error('[__NAME__] ' + message);
   }
-}
+};
 
-export function $warn(message: string): void {
+export const $warn = (message: string): void => {
   console.warn('[__NAME__] ' + message);
-}
+};
